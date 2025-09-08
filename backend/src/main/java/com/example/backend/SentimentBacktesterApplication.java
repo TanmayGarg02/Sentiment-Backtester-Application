5d8c93dd032cb670825eb21047f58c67f6f6e806
TanmayGarg02 <tanmaygarg021103@gmail.com>
2025-09-08 22:57:25 +0530

    first commit

---
+++ backend/src/main/java/com/example/backend/SentimentBacktesterApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentimentBacktesterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentimentBacktesterApplication.class, args);
+	}
+
+}
